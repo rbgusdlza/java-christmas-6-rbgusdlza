bfbe110f5f42ae957af44d13645224567e3d2584
rbgusdlza <rbgusdlza@gmail.com>
2023-11-13 02:43:36 +0900

    feat(Menu): 음식들의 정보가 담긴 Menu 클래스에 음식 이름 정보 추가

--- src/main/java/christmas/domain/DiscountCasher.java
+++ src/main/java/christmas/domain/DiscountCasher.java
@@ -1,12 +1,19 @@
 package christmas.domain;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class DiscountCasher {
     private Map<Menu, Integer> orderDetails = new HashMap<>();
 
-    public void makeOrderDetails(String InputOrder) {
+    public void makeOrderDetails(String inputOrders) {
+        String[] inputOrder = inputOrders.split(",");
+        for (String item : inputOrder) {
+            String[] subItems = item.split("-");
+            
+        }
 
     }
 }
--- src/main/java/christmas/domain/Menu.java
+++ src/main/java/christmas/domain/Menu.java
@@ -1,24 +1,26 @@
 package christmas.domain;
 
 public enum Menu {
-    MUSHROOM_SOUP("appetizer", 6_000),
-    TAPAS("appetizer", 5_500),
-    CAESAR_SALAD("appetizer", 8_000),
-    T_BONE_STEAK("main", 55_000),
-    BARBECUE_RIBS("main", 54_000),
-    SEAFOOD_PASTA("main", 35_000),
-    XMAS_PASTA("main", 25_000),
-    CHOCOLATE_CAKE("desert", 15_000),
-    ICE_CREAM("desert", 5_000),
-    ZERO_COKE("beverage", 3_000),
-    RED_WINE("beverage", 60_000),
-    CHAMPAGNE("beverage", 25_000);
+    MUSHROOM_SOUP("appetizer", 6_000, "양송이수프"),
+    TAPAS("appetizer", 5_500, "타파스"),
+    CAESAR_SALAD("appetizer", 8_000, "시저샐러드"),
+    T_BONE_STEAK("main", 55_000, "티본스테이크"),
+    BARBECUE_RIBS("main", 54_000, "바비큐립"),
+    SEAFOOD_PASTA("main", 35_000, "해산물파스타"),
+    XMAS_PASTA("main", 25_000, "크리스마스파스타"),
+    CHOCOLATE_CAKE("desert", 15_000, "초코케이크"),
+    ICE_CREAM("desert", 5_000, "아이스크림"),
+    ZERO_COKE("beverage", 3_000, "제로콜라"),
+    RED_WINE("beverage", 60_000, "레드와인"),
+    CHAMPAGNE("beverage", 25_000, "샴페인");
 
     private final String course;
     private final int price;
+    private final String name;
 
-    private Menu(String course, int price) {
+    private Menu(String course, int price, String name) {
         this.course = course;
         this.price = price;
+        this.name = name;
     }
 }
