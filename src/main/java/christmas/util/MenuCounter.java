6c6fe4791409209b1a836faf0fcf430f0cdd799a
rbgusdlza <rbgusdlza@gmail.com>
2023-11-13 07:05:41 +0900

    feat(MenuCounter): 주문 내역에서 특정 타입에 해당하는 음식 개수를 알려주는 countFoodTypes 메서드 구현

--- src/main/java/christmas/util/MenuCounter.java
+++ src/main/java/christmas/util/MenuCounter.java
@@ -2,12 +2,21 @@ package christmas.util;
 
 import christmas.domain.Menu;
 import java.util.Map;
+import java.util.Objects;
 
 public class MenuCounter {
     private MenuCounter() {
     }
 
     public static int countFoodTypes(Map<Menu, Integer> orderDetails, String typeName) {
-
+        int typeCount = 0;
+        for (Map.Entry<Menu, Integer> orderEntry : orderDetails.entrySet()) {
+            Menu menu = orderEntry.getKey();
+            Integer menuCount = orderEntry.getValue();
+            if (Objects.equals(menu.getType(), typeName)) {
+                typeCount += menuCount;
+            }
+        }
+        return typeCount;
     }
 }
