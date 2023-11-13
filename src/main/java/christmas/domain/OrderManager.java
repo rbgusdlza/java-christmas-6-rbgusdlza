9c7cb161c60b4209b44241041cb0e571a94b4fa8
rbgusdlza <rbgusdlza@gmail.com>
2023-11-13 21:50:14 +0900

    feat(OutputView): 사용자에게 메시지를 출력하는 OutputView 클래스 메서드 구현

--- src/main/java/christmas/domain/OrderManager.java
+++ src/main/java/christmas/domain/OrderManager.java
@@ -33,6 +33,4 @@ public class OrderManager {
         }
         return typeCount;
     }
-
-    
 }
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,7 +1,14 @@
 package christmas.view;
 
+import static christmas.constant.MessageCatalog.BENEFIT_SUMMARY_MESSAGE;
+import static christmas.constant.MessageCatalog.ESTIMATED_PAYMENT_AMOUNT_MESSAGE;
+import static christmas.constant.MessageCatalog.EVENT_BADGE_MESSAGE;
+import static christmas.constant.MessageCatalog.FREE_MENU_MESSAGE;
 import static christmas.constant.MessageCatalog.GREETING;
+import static christmas.constant.MessageCatalog.ORDER_MENU_MESSAGE;
 import static christmas.constant.MessageCatalog.REQUEST_VISIT_DAY;
+import static christmas.constant.MessageCatalog.TOTAL_DISCOUNT_AMOUNT_MESSAGE;
+import static christmas.constant.MessageCatalog.TOTAL_PURCHASE_AMOUNT_MESSAGE;
 
 public class OutputView {
     private final int EVENT_MONTH = 12;
@@ -15,4 +22,31 @@ public class OutputView {
         System.out.printf(REQUEST_VISIT_DAY + LINE_DIVIDER, EVENT_MONTH);
     }
 
+    public void printMenu() {
+        System.out.println(ORDER_MENU_MESSAGE);
+    }
+
+    public void printTotalPurchaseAmount() {
+        System.out.println(TOTAL_PURCHASE_AMOUNT_MESSAGE);
+    }
+
+    public void printFreeMenu() {
+        System.out.println(FREE_MENU_MESSAGE);
+    }
+
+    public void printBenefit() {
+        System.out.println(BENEFIT_SUMMARY_MESSAGE);
+    }
+
+    public void printTotalDiscountAmount() {
+        System.out.println(TOTAL_DISCOUNT_AMOUNT_MESSAGE);
+    }
+
+    public void printPaymentAmount() {
+        System.out.println(ESTIMATED_PAYMENT_AMOUNT_MESSAGE);
+    }
+
+    public void printBadge() {
+        System.out.printf(EVENT_BADGE_MESSAGE + LINE_DIVIDER, EVENT_MONTH);
+    }
 }
