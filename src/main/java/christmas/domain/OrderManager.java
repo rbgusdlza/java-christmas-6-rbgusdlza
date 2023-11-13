43ad9b781e5a55f2a589c8042f07cba54cb7193f
rbgusdlza <rbgusdlza@gmail.com>
2023-11-13 21:21:36 +0900

    fix: 주문 내역에서 메뉴 종류를 세던 util 클래스 제거 및 해당 기능을 OrderManager 클래스 메서드로 구현

--- src/main/java/christmas/domain/OrderManager.java
+++ src/main/java/christmas/domain/OrderManager.java
@@ -2,6 +2,7 @@ package christmas.domain;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class OrderManager {
     private Map<Menu, Integer> orderDetails;
@@ -21,4 +22,17 @@ public class OrderManager {
         return totalPurchaseAmount;
     }
 
+    public int countFoodTypes(Map<Menu, Integer> orderDetails, String typeName) {
+        int typeCount = 0;
+        for (Map.Entry<Menu, Integer> orderEntry : orderDetails.entrySet()) {
+            Menu menu = orderEntry.getKey();
+            Integer menuCount = orderEntry.getValue();
+            if (Objects.equals(menu.getType(), typeName)) {
+                typeCount += menuCount;
+            }
+        }
+        return typeCount;
+    }
+
+    
 }
