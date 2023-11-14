0a12d59389e7a40de7a85ba0333272df42ddfcd1
rbgusdlza <rbgusdlza@gmail.com>
2023-11-15 05:44:25 +0900

    feat(MenuTest): Menu Enum을 테스트하는 MenuTest 클래스 생성 및 메서드 구현

--- src/test/java/christmas/domain/BadgeTest.java
+++ src/test/java/christmas/domain/BadgeTest.java
@@ -6,28 +6,28 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BadgeTest {
-    @DisplayName("첫번째 기준 금액보다 작은 경우 어떤 뱃지를 받을지 테스트한다.")
+    @DisplayName("첫번째 기준 금액보다 작은 경우 어떤 배지를 받을지 테스트한다.")
     @Test
     public void testBadgeCreationNoMerchandise() {
         Badge badge = new Badge(0);
         assertEquals("없음", badge.toString());
     }
 
-    @DisplayName("두번째 기준 금액보다 작은 경우 어떤 뱃지를 받을지 테스트한다.")
+    @DisplayName("두번째 기준 금액보다 작은 경우 어떤 배지를 받을지 테스트한다.")
     @Test
     public void testBadgeCreationSecondThresholdMerchandise() {
         Badge badge = new Badge(8_000);
         assertEquals("별", badge.toString());
     }
 
-    @DisplayName("세번째 기준 금액보다 작은 경우 어떤 뱃지를 받을지 테스트한다.")
+    @DisplayName("세번째 기준 금액보다 작은 경우 어떤 배지를 받을지 테스트한다.")
     @Test
     public void testBadgeCreationThirdThresholdMerchandise() {
         Badge badge = new Badge(15_000);
         assertEquals("트리", badge.toString());
     }
 
-    @DisplayName("세번째 기준 금액보다 높은 경우 어떤 뱃지를 받을지 테스트한다.")
+    @DisplayName("세번째 기준 금액보다 높은 경우 어떤 배지를 받을지 테스트한다.")
     @Test
     public void testBadgeCreationFourthThresholdMerchandise() {
         Badge badge = new Badge(25_000);
